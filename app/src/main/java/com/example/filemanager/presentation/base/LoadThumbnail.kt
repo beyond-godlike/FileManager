82fad0c86957366fb1e74b7c21375a35284ea430
Dia <dia_the_ghost@mail.ru>
2024-12-29 18:22:45 +0300

    issue with thumbnail loading fixed

--- app/src/main/java/com/example/filemanager/presentation/base/LoadThumbnail.kt
+++ app/src/main/java/com/example/filemanager/presentation/base/LoadThumbnail.kt
@@ -11,13 +11,15 @@ import androidx.core.graphics.drawable.toBitmap
 import com.example.filemanager.R
 import com.example.filemanager.data.repository.MediaItem
 
-
-//https://stackoverflow.com/questions/58030463/album-art-column-is-deprecated-from-api-29-and-so-on-how-to-obtain-path
 @Composable
 fun getThumbnail(context: Context, item: MediaItem, size: Size) : Bitmap {
     val thumbnail: Bitmap? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        context.contentResolver
-            .loadThumbnail(item.contentUri.normalizeScheme(), size, null)
+        val bitmap = try {
+            context.contentResolver.loadThumbnail(item.contentUri, size, null)
+        } catch (e: Exception) {
+            ContextCompat.getDrawable(context, R.drawable.placeholder_image)!!.toBitmap()
+        }
+        bitmap
     } else  {
         MediaStore.Images.Thumbnails.getThumbnail(context.contentResolver, item.id, MediaStore.Images.Thumbnails.MINI_KIND, null)
     }
